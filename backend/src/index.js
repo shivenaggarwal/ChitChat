facf5a7ee3a732aab9faa51768df0bc0f2e11065
shivenaggarwal <shivenagg04@gmail.com>
2025-03-07 23:03:17 +0530

    frontend profile page and added upload profile pic functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,14 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
